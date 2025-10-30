4c7680642ee778a7a85bdf108c5870d604a3d193
Jaime Garcia <jaimeghe2@gmail.com>
2025-10-30 08:52:12 +0000

    consts declared

---
+++ app.js
@@ -0,0 +1,7 @@
+const container = document.querySelector('#container');
+const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/refs/heads/master/sprites/pokemon'
+const themeBtn = document.querySelector('#toggleTheme');
+const knob = document.querySelector('.knob');
+const searchInput = document.querySelector('#searchInput');
+const loadingScreen = document.querySelector('#loading');
+const TOTAL = document.querySelector('#loading');
